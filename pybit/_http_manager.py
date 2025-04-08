b2bf82ba0411f906afe143abb9564d86b9ff6142
dextertd <dexterdickinson@protonmail.com>
2025-04-08 20:40:54 +0400

    fix retry_codes and ignore_codes

--- pybit/_http_manager.py
+++ pybit/_http_manager.py
@@ -72,14 +72,8 @@ class _V5HTTPManager:
     timeout: int = field(default=10)
     recv_window: bool = field(default=5000)
     force_retry: bool = field(default=False)
-    retry_codes: defaultdict[dict] = field(
-        default_factory=dict,
-        init=False,
-    )
-    ignore_codes: dict = field(
-        default_factory=dict,
-        init=False,
-    )
+    retry_codes: defaultdict[dict] = field(default_factory=dict)
+    ignore_codes: dict = field(default_factory=dict)
     max_retries: bool = field(default=3)
     retry_delay: bool = field(default=3)
     referral_id: bool = field(default=None)
